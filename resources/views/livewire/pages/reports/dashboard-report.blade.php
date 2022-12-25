1e414231146f95d640fb647919886db76b117270
dean <dean@bukuku.solutions>
2022-12-25 11:19:14 +0700

    Add laporan

--- resources/views/livewire/pages/reports/dashboard-report.blade.php
+++ resources/views/livewire/pages/reports/dashboard-report.blade.php
@@ -7,9 +7,8 @@
                     <div class="col-12"><h2 class="content-header-title float-left pr-1 mb-0"> Pemesanan Kios</h2>
                         <div class="breadcrumb-wrapper">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#"
-                                                               wire:click="link('/dashboard')"
-                                                               class="router-link-active" target="_self">
+                                <li class="breadcrumb-item "><a href="{{ url('/dashboard') }}"
+                                                                class="router-link-active" target="_self">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px"
                                              viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                              stroke-linecap="round" stroke-linejoin="round"
@@ -18,9 +17,10 @@
                                             <polyline points="9 22 9 12 15 12 15 22"></polyline>
                                         </svg>
                                     </a></li>
-                                <li class="breadcrumb-item"><a href="#" target="_self" class=""> Forms </a></li>
+{{--                                <li class="breadcrumb-item"><a href="{{ url('/product/order') }}" target="_self"--}}
+{{--                                                               class=""> Pemesanan kios </a></li>--}}
                                 <li class="breadcrumb-item active"><span
-                                        aria-current="location"> Pemesanan Kios</span></li>
+                                        aria-current="location"> Laporan</span></li>
                             </ol>
                         </div>
                     </div>
@@ -28,63 +28,23 @@
             </div>
             <div class="content-header-right text-md-right d-md-block d-none mb-1 col-md-3 col-12">
                 <div class="dropdown b-dropdown btn-group" id="__BVID__2011"><!---->
-                    <button aria-haspopup="true" aria-expanded="false" type="button"
-                            class="btn dropdown-toggle btn-link p-0 dropdown-toggle-no-caret"
-                            id="__BVID__2011__BV_toggle_">
-                        <button type="button" class="btn btn-icon btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="feather feather-settings">
-                                <circle cx="12" cy="12" r="3"></circle>
-                                <path
-                                    d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
-                            </svg>
-                        </button>
-                    </button>
-                    <ul role="menu" tabindex="-1" class="dropdown-menu dropdown-menu-right"
-                        aria-labelledby="__BVID__2011__BV_toggle_">
-                        <li role="presentation"><a href="/demo/vuexy-vuejs-admin-dashboard-template/demo-2/apps/todo"
-                                                   class="dropdown-item" role="menuitem" target="_self">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24"
+                    <div class="content-header-right text-md-right d-md-block d-none mb-1 col-md-3 col-12">
+                        <a href="{{url('/logout')}}">
+                            <button type="button" class="btn btn-icon btn-primary">
+                                <svg width="14px" height="14px" viewBox="0 0 24 24"
                                      fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                     stroke-linejoin="round" class="feather feather-check-square">
-                                    <polyline points="9 11 12 14 22 4"></polyline>
-                                    <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
-                                </svg>
-                                <span class="align-middle ml-50">Todo</span></a></li>
-                        <li role="presentation"><a href="/demo/vuexy-vuejs-admin-dashboard-template/demo-2/apps/chat"
-                                                   class="dropdown-item" role="menuitem" target="_self">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24"
-                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                     stroke-linejoin="round" class="feather feather-message-square">
-                                    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                                </svg>
-                                <span class="align-middle ml-50">Chat</span></a></li>
-                        <li role="presentation"><a href="/demo/vuexy-vuejs-admin-dashboard-template/demo-2/apps/email"
-                                                   class="dropdown-item" role="menuitem" target="_self">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24"
-                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                     stroke-linejoin="round" class="feather feather-mail">
+                                     stroke-linejoin="round" class="feather feather-settings">
+                                    <circle cx="12" cy="12" r="3"></circle>
                                     <path
-                                        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                                    <polyline points="22,6 12,13 2,6"></polyline>
+                                        d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
                                 </svg>
-                                <span class="align-middle ml-50">Email</span></a></li>
-                        <li role="presentation"><a
-                                href="/demo/vuexy-vuejs-admin-dashboard-template/demo-2/apps/calendar"
-                                class="dropdown-item" role="menuitem" target="_self">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24"
-                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                     stroke-linejoin="round" class="feather feather-calendar">
-                                    <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                    <line x1="16" y1="2" x2="16" y2="6"></line>
-                                    <line x1="8" y1="2" x2="8" y2="6"></line>
-                                    <line x1="3" y1="10" x2="21" y2="10"></line>
-                                </svg>
-                                <span class="align-middle ml-50">Calendar</span></a></li>
-                    </ul>
+
+                            </button>
+                        </a>
+                    </div>
                 </div>
             </div>
+
         </div>
 
         <div class="card p-2">
